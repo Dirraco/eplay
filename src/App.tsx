af75c64951e30429cd01e595c51726e6699a2054
Diego Santos <diego449diego@gmail.com>
2024-08-03 17:18:43 -0300

    03-08

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,3 @@
-// import {
-//   // createBrowserRouter,
-//   // RouterProvider,
-//   BrowserRouter,
-//   Routes,
-//   Route
-// } from 'react-router-dom'
-
-// import Header from './components/Header'
-// import { GlobalCss } from './styles'
-// import Home from './pages/Home'
-// import Categories from './pages/Categories'
-
-// const Rotas = () => (
-//   <Routes>
-//     <Route path="/" element={<Home />} />
-//     <Route path="/categories" element={<Categories />} />
-//   </Routes>
-// )
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <GlobalCss />
-//       <div className="container">
-//         <Header />
-//       </div>
-//       <Rotas />
-//     </BrowserRouter>
-//   )
-// }
-
-// export default App
-
 import { BrowserRouter } from 'react-router-dom'
 
 import Header from './components/Header'
